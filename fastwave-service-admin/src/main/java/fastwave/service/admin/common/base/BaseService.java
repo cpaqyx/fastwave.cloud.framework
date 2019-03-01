fe9e907819250aa79377c5fe1d316c20447abe12
fastwave <cpaqyx@aliyun.com>
2019-03-01 17:42:35 +0800

    添加dockerfile文件

--- fastwave-service-admin/src/main/java/fastwave/service/admin/common/base/BaseService.java
+++ fastwave-service-admin/src/main/java/fastwave/service/admin/common/base/BaseService.java
@@ -7,10 +7,7 @@ import org.springframework.stereotype.Service;
  * @ClassName:  BaseService   
  * @Description:Base服务   
  * @author: fastwave
- * @date:   2018年5月12日 上午10:05:16   
- *     
- * @Copyright: 2018 fastwave. All rights reserved. 
- * 注意：本内容仅限于信翰创武汉物联科技有限公司内部传阅，禁止外泄以及用于其他的商业目
+ * @date:   
  */
 @Service
 public class BaseService {
